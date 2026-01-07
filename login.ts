f857dd318f55c03aa8712b01e9964f60819e258e
Het <hra.opash@gmail.com>
2026-01-07 12:32:55 +0530

    .

--- login.ts
+++ login.ts
@@ -4,7 +4,7 @@ const form = document.querySelector(".form") as HTMLFormElement;
 const emailPattern = new RegExp(
   /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
 );
-form.addEventListener("submit", (e) => {
+form.addEventListener("submit", (e: Event) => {
   e.preventDefault();
   const emailValue = email.value;
   const passwordValue = password.value;
