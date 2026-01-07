44e480dd81d778995d296d3d9499a80360f566d6
Het <hra.opash@gmail.com>
2026-01-07 13:04:56 +0530

    .

--- login.js
+++ login.js
@@ -1,12 +1,21 @@
 var email = document.getElementById("email");
 var password = document.getElementById("password");
 var form = document.querySelector(".form");
+var username = document.getElementById("username");
+var phoneNumber = document.getElementById("phone");
+var bod = document.getElementById("bod");
 var emailPattern = new RegExp(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/);
+var phonePattern = new RegExp(/^\d{10}$/);
 form.addEventListener("submit", function (e) {
     e.preventDefault();
     var emailValue = email.value;
     var passwordValue = password.value;
-    if (emailValue === "" || passwordValue === "") {
+    var phoneValue = phoneNumber.value;
+    var usernameValue = username.value;
+    if (emailValue === "" ||
+        passwordValue === "" ||
+        phoneValue === "" ||
+        usernameValue === "") {
         alert("Please fill in all fields...!");
         return;
     }
@@ -18,4 +27,10 @@ form.addEventListener("submit", function (e) {
         alert("Please Provide valid email...");
         return;
     }
+    if (!phonePattern.test(phoneValue)) {
+        alert("Enter Valid Phone Number...");
+        return;
+    }
+    alert("Login Successful...");
+    form.submit();
 });
--- login.ts
+++ login.ts
@@ -1,14 +1,30 @@
 const email = document.getElementById("email") as HTMLInputElement;
 const password = document.getElementById("password") as HTMLInputElement;
 const form = document.querySelector(".form") as HTMLFormElement;
+const username = document.getElementById("username") as HTMLInputElement;
+const phoneNumber = document.getElementById("phone") as HTMLInputElement;
+const bod = document.getElementById("bod") as HTMLInputElement;
+
 const emailPattern = new RegExp(
   /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
 );
+
+const phonePattern = new RegExp(/^\d{10}$/);
+
 form.addEventListener("submit", (e: Event) => {
   e.preventDefault();
+
   const emailValue = email.value;
   const passwordValue = password.value;
-  if (emailValue === "" || passwordValue === "") {
+  const phoneValue = phoneNumber.value;
+  const usernameValue = username.value;
+
+  if (
+    emailValue === "" ||
+    passwordValue === "" ||
+    phoneValue === "" ||
+    usernameValue === ""
+  ) {
     alert("Please fill in all fields...!");
     return;
   }
@@ -20,4 +36,11 @@ form.addEventListener("submit", (e: Event) => {
     alert("Please Provide valid email...");
     return;
   }
+  if (!phonePattern.test(phoneValue)) {
+    alert("Enter Valid Phone Number...");
+    return;
+  }
+
+  alert("Login Successful...");
+  form.submit();
 });
